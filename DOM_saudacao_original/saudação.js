c512ba5a16c33afcc667669419a8920938049516
Douglasjrdev <114544394+Douglasjrdev@users.noreply.github.com>
2024-08-01 03:03:20 -0300

    fiz alguns ex

--- DOM_saudacao_original/saudação.js
+++ DOM_saudacao_original/saudação.js
@@ -1,6 +1,11 @@
-const nomeUsuario = 'Douglas';
+let valor = document.getElementById('txtEmail')
+let msg = document.querySelector('.feed')
 
-(function(){
-    const elemento = document.querySelector('.top-bar p');
-    elemento.textContent += nomeUsuario
-})()
+
+function cadastro(){
+    let ValorDoInput = valor.value
+    msg.style.color = 'red'
+    msg.innerHTML = `O email ${ValorDoInput} foi cadastrado com sucesso`
+    
+
+}
