fb3fff13b8fb11757f5da61d6250a5fc5af4dfa5
Douglasjrdev <114544394+Douglasjrdev@users.noreply.github.com>
2025-10-28 21:01:32 -0300

    promisses

---
+++ HORA DE CODAR JS/ASSICRONA/assicrona.js
@@ -0,0 +1,57 @@
+
+//SetTimeout
+
+
+
+
+console.log("ainda não executou!");
+
+
+setTimeout(() => {
+    console.log("executou depois de 2 sec")
+}, 2000);
+
+
+console.log("ainda não executou 2");
+
+
+
+
+//SetInterval
+
+console.log("ainda não executou!");
+
+
+// setInterval(() => {
+//     console.log("executou!");
+// }, 3000);
+
+
+
+console.log("ainda não executou!2");
+
+
+//Promisses
+
+const promessa = Promise.resolve(5 + 5);
+
+
+console.log('algum codigo')
+
+
+
+promessa.then((Value) => {
+    console.log(`A soma é ${Value}`)
+})
+
+console.log('outro codigo')
+
+//falha na promisse
+
+Promise.resolve(4*'asas')
+.then((n) => {
+    if(Number.isNaN(n)){
+        throw new Error ('Valor invalido')
+    }
+})
+.catch((err) => console.log('um erro ocorreu ' + err))
