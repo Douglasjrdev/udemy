cc59e6938365f3c042c7e1e46a82f8b5d771c149
Douglasjrdev <114544394+Douglasjrdev@users.noreply.github.com>
2025-11-18 22:04:29 -0300

    aprendi a usar eventos

--- HORA DE CODAR JS/Eventos/script.js
+++ HORA DE CODAR JS/Eventos/script.js
@@ -30,4 +30,41 @@ btn.addEventListener("click", function() {
 
  const mytititle = document.querySelector("#title")
 
- mytititle.addEventListener("click"
+ mytititle.addEventListener("click", function(event){
+   console.log(event.target)
+   console.log(event)
+ })
+
+ //stop progragação
+
+ const contbtn = document.querySelector("#container-btn")
+
+ const divbtn = document.querySelector("#div-btn")
+
+ contbtn.addEventListener("click", ()=> {
+   console.log("evento 1")
+ })
+
+ divbtn.addEventListener("click", (e)=> {
+   e.stopPropagation() //tem que chamar o () para executar
+   console.log("evento 2")
+ })
+
+ //cancelando eventos padrão
+
+ const a = document.querySelector("a")
+
+ a.addEventListener("click", (e)=>{
+   e.preventDefault()
+   console.log("parou o evento")
+ })
+
+ // pegando teclas
+
+ document.addEventListener("keyup", (e)=> {
+   console.log(`soltou a tecla ${e.key}`)
+ })
+
+ document.addEventListener("keydown", (e)=> {
+   console.log(`apertou a tecla ${e.key}`)
+ })
